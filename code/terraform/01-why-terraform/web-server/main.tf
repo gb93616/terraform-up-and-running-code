fc6dfedcc0b35488b10935e64568cd7aa279bba7
Graham Bell <gb93616@gmail.com>
2021-06-04 14:42:38 +0100

    commented out version check

--- code/terraform/01-why-terraform/web-server/main.tf
+++ code/terraform/01-why-terraform/web-server/main.tf
@@ -1,13 +1,13 @@
 #
 terraform {
-  required_version = ">= 0.12, < 0.13"
+  required_version = ">= 0.12, < 0.16"
 }
 
 provider "aws" {
   region = "us-east-2"
 
   # Allow any 2.x version of the AWS provider
-  version = "~> 2.0"
+  #version = "~> 2.0"
 }
 
 resource "aws_instance" "app" {
